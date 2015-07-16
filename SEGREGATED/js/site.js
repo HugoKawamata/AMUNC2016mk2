dcb5667ebc59dbfb34e2116e41949d570f20c4c5
wolftongue <hugokawamata@gmail.com>
2015-07-16 12:10:39 +1000

    Fixed navbar mobile scaling (sort of)

--- SEGREGATED/js/site.js
+++ SEGREGATED/js/site.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
 
+		$(".navbar-toggle").click(function(){
+			if ($("#navbar").css("aria-expanded", "true") == true){
+				
+				console.log("collapse")
+				;$("#navbar").collapse("hide");
+			};
+		});
+
 	//-------------------NAVIGATION TEXT COLOUR-------------------//
 		//The dropdown nav buttons will retain white text when their respective menus are hovered over
 	    $(".dropdown-menu").mouseenter(function(){
@@ -19,21 +27,43 @@ $( document ).ready(function() {
 	 	//When the site goes into mobile view, the logo will disappear
 		 $(window).resize(function(){	
 		 	if ($(window).width() < 768) {
-			   $(".logo-container").hide();
-			   console.log("hide+move");
-			   $(".dropdown-menu").css({"position": "absolute", "top": "80px"});
+			   $(".logo-container").hide(); //Hide logo
+			   $(".navbar > .container-fluid").css({"min-width": "0px"}); //Resize navbar container
+			   $(".navbar-collapse > .navbar-nav").css({ //Resize navbar elements
+				   	"width": "auto",
+				   	"min-width": "0px"
+				   });
+			   $(".navbar-collapse > .navbar-nav > li").css({ //Resize navbar elements
+			   		"width": "auto",
+			   		"min-width": "0px",
+			   		"overflow": "visible"
+			   });
+
 			}
 			else {
 			   $(".logo-container").show();
 			}
 		});
 		//If the site starts in mobile view, the logo will disappear
-		if ($(window).width() < 768) {
-		   $(".logo-container").hide();
-		}
-		else {
-		   $(".logo-container").show();
-		}
+		 	if ($(window).width() < 768) {
+			   $(".logo-container").hide(); //Hide logo
+			   $(".navbar > .container-fluid").css({"min-width": "0px"}); //Resize navbar container
+			   $(".navbar-collapse > .navbar-nav").css({ //Resize navbar elements
+				   	"width": "auto",
+				   	"min-width": "0px"
+				   });
+			   $(".navbar-collapse > .navbar-nav > li").css({ //Resize navbar elements
+			   		"width": "auto",
+			   		"min-width": "0px",
+			   		"overflow": "visible"
+			   });
+
+			}
+			else {
+			   $(".logo-container").show();
+			}
+	//-------------------NAVBAR COLLAPSE FIX-------------------//
+		
 
 
 });
